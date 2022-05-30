c021f6f62994e9f41b1a1d334f273685a3cd7d16
kanini-esther <esther.njeri.kanini@gmail.com>
2022-05-30 17:29:09 +0300

    calculations

---
+++ classarea.py
@@ -0,0 +1,15 @@
+
+class Rectangle:
+    def _init_ (self.Area)
+    self.Area = ('l * W')
+    Al = 4
+    Aw = 6
+print('Al*Aw') 
+
+
+# l = float(input('Enter length: '))
+# w = float(input('Enter width: '))
+
+# # area = 
+# area = l*w
+# print('The area of the rectangle is %0.2f' %area)
